14e7d0dc6abe5359ba4e1a23e59974b927edb57d
ronoppenheimer <ronoppenheimer@gmail.com>
2015-09-29 19:53:47 -0700

    Added a countdown timer

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -9,14 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timerCount = 60
+    var timerRunning = false
+    var timer = NSTimer()
 
     @IBOutlet var nameLabel: UILabel!
+    
+    
+    @IBOutlet var timerLabel: UILabel!
+    func Counting() {
+        if timerCount > 0 {
+        timerCount -= 1
+        timerLabel.text = "\(timerCount)"
+        }
+        
+    }
+    
+    
+    @IBAction func startButton(sender: UIButton) {
+        if timerRunning == false
+        {
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("Counting"), userInfo: nil, repeats: true)
+            timerRunning = true
+        }
+    }
+
+    @IBAction func stopButton(sender: UIButton) {
+        if timerRunning == true
+        {
+            timer.invalidate()
+            timerRunning = false
+        }
+    }
+    
+    @IBAction func restartButton(sender: UIButton) {
+        timerCount = 60
+        timerLabel.text = "60"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
