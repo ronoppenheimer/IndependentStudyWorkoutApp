8fb194ba371b9b5542ff9bda09fd1b0099129c37
Students <students@techarts-mbp-11.lwhs.org>
2015-11-13 08:48:59 -0800

    Added rest and then second exercise

--- HelloWorld/HelloWorld/restViewController.swift
+++ HelloWorld/HelloWorld/restViewController.swift
@@ -1,17 +1,17 @@
 //
-//  exerciseViewController.swift
+//  restViewController.swift
 //  HelloWorld
 //
-//  Created by Students on 11/9/15.
+//  Created by Students on 11/13/15.
 //  Copyright © 2015 RonOppenheimer. All rights reserved.
 //
 
 import UIKit
 import AVFoundation
 
-class exerciseViewController: UIViewController {
+class restViewController: UIViewController {
 
-    var timerCount = 50
+    var timerCount = 10
     var timerRunning = true
     var timer = NSTimer()
     
@@ -21,6 +21,7 @@ class exerciseViewController: UIViewController {
         super.viewDidLoad()
         Timer()
         // Do any additional setup after loading the view, typically from a nib.
+        progressBar.setProgress(0, animated: true)
     }
     
     override func didReceiveMemoryWarning() {
@@ -29,14 +30,13 @@ class exerciseViewController: UIViewController {
     }
     
     func Timer() {
-        if timerRunning == true {
         timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("Counting"), userInfo: nil, repeats: true)
         playSound()
-        }
     }
     
+    
     @IBOutlet var nameLabel: UILabel!
-
+    
     
     @IBOutlet var timerLabel: UILabel!
     func Counting() {
@@ -44,23 +44,24 @@ class exerciseViewController: UIViewController {
             timerCount -= 1
             timerLabel.text = "\(timerCount)"
             audioPlayer!.play()
-            let progressCount = 50 - timerCount
-            let fractionalProgress = Float(progressCount) / 50
+            let progressCount = 10 - timerCount
+            let fractionalProgress = Float(progressCount) / 10
             let animated = progressCount != 0
             progressBar.setProgress(fractionalProgress, animated: animated)
         }
         else {
-            performSegueWithIdentifier("goToRest", sender: nil)
+            performSegueWithIdentifier("goToExercise", sender: nil)
         }
-        
     }
-
+    
+    
     @IBOutlet var progressBar: UIProgressView!
-
+    
     
     func timerStop() {
         timer.invalidate()
         timerRunning = false
+        
     }
     
     func playSound() {
@@ -90,45 +91,21 @@ class exerciseViewController: UIViewController {
         
     }
     
-    @IBAction func startButton(sender: UIButton) {
-        if timerRunning == false
-        {
-            timerRunning = true
-            Timer()
-            sender.setTitle("Stop", forState: UIControlState.Normal)
-        }
-        else
-        {
-            timerStop()
-            sender.setTitle("Start", forState: UIControlState.Normal)
-        }
-        
-    }
-    
-    
-    @IBAction func restartButton(sender: UIButton) {
-        timerStop()
-        timerCount = 50
-        timerLabel.text = "50"
-    }
-    
-
     @IBAction func exitButton(sender: UIButton) {
         timerStop()
-        timerCount = 0
-        performSegueWithIdentifier("exerciseGoToStart", sender: nil)
+        performSegueWithIdentifier("restGoToStart", sender: nil)
     }
-
     
-
+    
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
