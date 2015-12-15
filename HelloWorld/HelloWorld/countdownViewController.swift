9ec16e3f70063db61bbd0ea7821d7fe8960b1f53
Students <students@techarts-mbp-11.lwhs.org>
2015-12-15 13:37:04 -0800

    cleaned up countdown view controller

--- HelloWorld/HelloWorld/countdownViewController.swift
+++ HelloWorld/HelloWorld/countdownViewController.swift
@@ -16,32 +16,12 @@ class countdownViewController: UIViewController {
     var timer = NSTimer()
 
     
-    var audioPlayer: AVAudioPlayer?
     var beginWith: AVAudioPlayer?
     var three: AVAudioPlayer?
     var two: AVAudioPlayer?
     var one: AVAudioPlayer?
-    var rest: AVAudioPlayer?
-    var nextUp: AVAudioPlayer?
-    var goodJob: AVAudioPlayer?
-    var success: AVAudioPlayer?
     var abdominalCrunch: AVAudioPlayer?
     var burpees: AVAudioPlayer?
-    var curvedLegsCrunch: AVAudioPlayer?
-    var heelRaises: AVAudioPlayer?
-    var jackKnife: AVAudioPlayer?
-    var legRaise: AVAudioPlayer?
-    var lunges: AVAudioPlayer?
-    var plankHold: AVAudioPlayer?
-    var pushUps: AVAudioPlayer?
-    var runningInPlace: AVAudioPlayer?
-    var russianTwist: AVAudioPlayer?
-    var scissors: AVAudioPlayer?
-    var sideCrunch: AVAudioPlayer?
-    var squats: AVAudioPlayer?
-    var tuckedVUps: AVAudioPlayer?
-    var vUps: AVAudioPlayer?
-    var windshieldWiper: AVAudioPlayer?
     
     
     override func viewDidLoad() {
@@ -82,52 +62,10 @@ class countdownViewController: UIViewController {
                 if nextUpLabel.text == "Next Up: Abdominal Crunch"{
                     abdominalCrunch?.play()
                 }
-                if nextUpLabel.text == "Next Up: Scissors"{
-                    scissors?.play()
-                }
-                if nextUpLabel.text == "Next Up: Curved Legs Crunch"{
-                    curvedLegsCrunch?.play()
-                }
-                if nextUpLabel.text == "Next Up: Leg Raise"{
-                    legRaise?.play()
-                }
-                if nextUpLabel.text == "Next Up: Side Crunch"{
-                    sideCrunch?.play()
-                }
-                if nextUpLabel.text == "Next Up: Windshield Wiper"{
-                    windshieldWiper?.play()
-                }
-                if nextUpLabel.text == "Next Up: Tucked V-Ups"{
-                    tuckedVUps?.play()
-                }
-                if nextUpLabel.text == "Next Up: Russian Twist"{
-                    russianTwist?.play()
-                }
                 if nextUpLabel.text == "Next Up: Burpees"{
                     burpees?.play()
                 }
-                if nextUpLabel.text == "Next Up: Plank Hold"{
-                    plankHold?.play()
-                }
-                if nextUpLabel.text == "Next Up: Squats"{
-                    squats?.play()
-                }
-                if nextUpLabel.text == "Next Up: V-Ups"{
-                    vUps?.play()
-                }
-                if nextUpLabel.text == "Next Up: Running In Place"{
-                    runningInPlace?.play()
-                }
-                if nextUpLabel.text == "Next Up: Lunges"{
-                    lunges?.play()
-                }
-                if nextUpLabel.text == "Next Up: Push-Ups"{
-                    pushUps?.play()
-                }
-                if nextUpLabel.text == "Next Up: Heel Raises"{
-                    heelRaises?.play()
-                }
-            }
+                            }
             if timerCount == 2 {
                 three?.play()
             }
@@ -160,32 +98,13 @@ class countdownViewController: UIViewController {
     func playSound() {
         
         // Set the sound file name & extension
-        let alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Tick", ofType: "mp3")!)
         let beginWithSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Begin With", ofType: "wav")!)
         let threeSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("3", ofType: "wav")!)
         let twoSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("2", ofType: "wav")!)
         let oneSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("1", ofType: "wav")!)
-        let restSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Rest", ofType: "wav")!)
-        let nextUpSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Next Up", ofType: "wav")!)
-        let goodJobSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Good Job", ofType: "wav")!)
-        let successSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Success", ofType: "wav")!)
         let abdominalCrunchSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Abdominal Crunch", ofType: "wav")!)
         let burpeesSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Burpees", ofType: "wav")!)
-        let curvedLegsCrunchSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Curved Legs Crunch", ofType: "wav")!)
-        let heelRaisesSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Heel Raises", ofType: "wav")!)
-        let jackKnifeSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Jack Knife", ofType: "wav")!)
-        let legRaiseSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Leg Raise", ofType: "wav")!)
-        let lungesSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Lunges", ofType: "wav")!)
-        let plankHoldSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Plank Hold", ofType: "wav")!)
-        let pushUpsSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Push-Ups", ofType: "wav")!)
-        let runningInPlaceSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Running In Place", ofType: "wav")!)
-        let russianTwistSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Russian Twist", ofType: "wav")!)
-        let scissorsSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Scissors", ofType: "wav")!)
-        let sideCrunchSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Side Crunch", ofType: "wav")!)
-        let squatsSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Squats", ofType: "wav")!)
-        let tuckedVUpsSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Tucked V-Ups", ofType: "wav")!)
-        let vUpsSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("V-Ups", ofType: "wav")!)
-        let windshieldWiperSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Windshield Wiper", ofType: "wav")!)
+        
         do {
             // Preperation
             try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient)
@@ -199,95 +118,30 @@ class countdownViewController: UIViewController {
         // Play the sound
         let error: NSError?
         do {
-            audioPlayer = try AVAudioPlayer(contentsOfURL: alertSound)
             
             beginWith = try AVAudioPlayer(contentsOfURL: beginWithSound)
             three = try AVAudioPlayer(contentsOfURL: threeSound)
             two = try AVAudioPlayer(contentsOfURL: twoSound)
             one = try AVAudioPlayer(contentsOfURL: oneSound)
-            rest = try AVAudioPlayer(contentsOfURL: restSound)
-            nextUp = try AVAudioPlayer(contentsOfURL: nextUpSound)
-            goodJob = try AVAudioPlayer(contentsOfURL: goodJobSound)
-            success = try AVAudioPlayer(contentsOfURL: successSound)
             abdominalCrunch = try AVAudioPlayer(contentsOfURL: abdominalCrunchSound)
             burpees = try AVAudioPlayer(contentsOfURL: burpeesSound)
-            curvedLegsCrunch = try AVAudioPlayer(contentsOfURL: curvedLegsCrunchSound)
-            heelRaises = try AVAudioPlayer(contentsOfURL: heelRaisesSound)
-            jackKnife = try AVAudioPlayer(contentsOfURL: jackKnifeSound)
-            legRaise = try AVAudioPlayer(contentsOfURL: legRaiseSound)
-            lunges = try AVAudioPlayer(contentsOfURL: lungesSound)
-            plankHold = try AVAudioPlayer(contentsOfURL: plankHoldSound)
-            pushUps = try AVAudioPlayer(contentsOfURL: pushUpsSound)
-            runningInPlace = try AVAudioPlayer(contentsOfURL: runningInPlaceSound)
-            russianTwist = try AVAudioPlayer(contentsOfURL: russianTwistSound)
-            scissors = try AVAudioPlayer(contentsOfURL: scissorsSound)
-            sideCrunch = try AVAudioPlayer(contentsOfURL: sideCrunchSound)
-            squats = try AVAudioPlayer(contentsOfURL: squatsSound)
-            tuckedVUps = try AVAudioPlayer(contentsOfURL: tuckedVUpsSound)
-            vUps = try AVAudioPlayer(contentsOfURL: vUpsSound)
-            windshieldWiper = try AVAudioPlayer(contentsOfURL: windshieldWiperSound)
             
         } catch let error1 as NSError {
             error = error1
-            audioPlayer = nil
             
             beginWith = nil
             three = nil
             two = nil
             one = nil
-            rest = nil
-            nextUp = nil
-            goodJob = nil
-            success = nil
             abdominalCrunch = nil
             burpees = nil
-            curvedLegsCrunch = nil
-            heelRaises = nil
-            jackKnife = nil
-            legRaise = nil
-            lunges = nil
-            plankHold = nil
-            pushUps = nil
-            runningInPlace = nil
-            russianTwist = nil
-            scissors = nil
-            sideCrunch = nil
-            squats = nil
-            tuckedVUps = nil
-            vUps = nil
-            windshieldWiper = nil
-            
         }
-        audioPlayer?.prepareToPlay()
         beginWith?.prepareToPlay()
         three?.prepareToPlay()
         two?.prepareToPlay()
         one?.prepareToPlay()
-        beginWith?.prepareToPlay()
-        three?.prepareToPlay()
-        two?.prepareToPlay()
-        one?.prepareToPlay()
-        rest?.prepareToPlay()
-        nextUp?.prepareToPlay()
-        goodJob?.prepareToPlay()
-        success?.prepareToPlay()
         abdominalCrunch?.prepareToPlay()
         burpees?.prepareToPlay()
-        curvedLegsCrunch?.prepareToPlay()
-        heelRaises?.prepareToPlay()
-        jackKnife?.prepareToPlay()
-        legRaise?.prepareToPlay()
-        lunges?.prepareToPlay()
-        plankHold?.prepareToPlay()
-        pushUps?.prepareToPlay()
-        runningInPlace?.prepareToPlay()
-        russianTwist?.prepareToPlay()
-        scissors?.prepareToPlay()
-        sideCrunch?.prepareToPlay()
-        squats?.prepareToPlay()
-        tuckedVUps?.prepareToPlay()
-        vUps?.prepareToPlay()
-        windshieldWiper?.prepareToPlay()
     }
     
     @IBAction func exitButton(sender: UIButton) {
